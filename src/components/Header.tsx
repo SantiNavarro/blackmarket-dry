276e3bb92cba8e6c9969c049ff95628aeaac4e73
santi <santiago.navarro@nan-labs.com>
2022-08-12 13:34:42 -0300

    Added custom hook for handling rezising and conditional rendering

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@
 import { useDispatch } from 'react-redux';
 import Switch from '@mui/material/Switch';
 import classNames from 'classnames/bind';
-import { BrowserView, MobileView } from 'react-device-detect';
+// import { isMobile } from 'react-device-detect';
 import { useNavigate } from 'react-router-dom';
 
 import SearchBar from './SearchBar';
@@ -12,14 +12,17 @@ import '../styles/containers/header.scss';
 import { useAppSelector } from '../store/hooks';
 import { selectThemeStatus } from '../store/features/theme/selectors';
 import MobileMenu from './MobileMenu';
+import { useIsSmallDevice } from '../hooks/useWindowSize';
 
 const Header = () => {
   const dispatch = useDispatch();
   const themeState = useAppSelector(selectThemeStatus);
   const navigate = useNavigate();
+  const isSmallDevice = useIsSmallDevice();
   const handleThemeChange = () => {
     dispatch(toggleTheme());
   };
+
   return (
     <div
       className={classNames({
@@ -27,32 +30,72 @@ const Header = () => {
         'header-theme-secondary': themeState,
       })}
     >
-      <BrowserView className="header-browser-view">
-        <div className="img-logo" onKeyDown={() => navigate('/')} onClick={() => navigate('/')} />
-        <SearchBar />
-        <NavigationSection />
-        <Switch
-          sx={{ backgroundColor: themeState ? '#f1af03' : 'grey', margin: '1rem 0' }}
-          checked={themeState}
-          onChange={handleThemeChange}
-          color="warning"
-        />
-      </BrowserView>
-      <MobileView className="header-mobile-view">
-        <div className="header-mobile-view__children">
+      {isSmallDevice ? (
+        <div className="header-mobile-view">
+          <div className="header-mobile-view__children">
+            <Switch
+              sx={{ backgroundColor: themeState ? '#f1af03' : 'grey', margin: '1rem 0' }}
+              checked={themeState}
+              onChange={handleThemeChange}
+              color="warning"
+            />
+            <div
+              className="img-logo"
+              onKeyDown={() => navigate('/')}
+              onClick={() => navigate('/')}
+            />
+            <MobileMenu />
+          </div>
+          <SearchBar />
+        </div>
+      ) : (
+        <div className="header-browser-view">
+          <div className="img-logo" onKeyDown={() => navigate('/')} onClick={() => navigate('/')} />
+          <SearchBar />
+          <NavigationSection />
           <Switch
             sx={{ backgroundColor: themeState ? '#f1af03' : 'grey', margin: '1rem 0' }}
             checked={themeState}
             onChange={handleThemeChange}
             color="warning"
           />
-          <div className="img-logo" onKeyDown={() => navigate('/')} onClick={() => navigate('/')} />
-          <MobileMenu />
         </div>
-        <SearchBar />
-      </MobileView>
+      )}
     </div>
   );
+  // return (
+  //   <div
+  //     className={classNames({
+  //       'header-container': true,
+  //       'header-theme-secondary': themeState,
+  //     })}
+  //   >
+  //     <BrowserView className="header-browser-view">
+  //       <div className="img-logo" onKeyDown={() => navigate('/')} onClick={() => navigate('/')} />
+  //       <SearchBar />
+  //       <NavigationSection />
+  //       <Switch
+  //         sx={{ backgroundColor: themeState ? '#f1af03' : 'grey', margin: '1rem 0' }}
+  //         checked={themeState}
+  //         onChange={handleThemeChange}
+  //         color="warning"
+  //       />
+  //     </BrowserView>
+  //     <MobileView className="header-mobile-view">
+  //       <div className="header-mobile-view__children">
+  //         <Switch
+  //           sx={{ backgroundColor: themeState ? '#f1af03' : 'grey', margin: '1rem 0' }}
+  //           checked={themeState}
+  //           onChange={handleThemeChange}
+  //           color="warning"
+  //         />
+  //         <div className="img-logo" onKeyDown={() => navigate('/')} onClick={() => navigate('/')} />
+  //         <MobileMenu />
+  //       </div>
+  //       <SearchBar />
+  //     </MobileView>
+  //   </div>
+  // );
 };
 
 export default Header;
